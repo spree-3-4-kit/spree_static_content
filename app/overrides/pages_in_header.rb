770f5e92cdc685e81a48b89cfead2eacdc88c6e0
Matthew Kennedy <m.kennedy@me.com>
2018-05-12 20:51:51 +0100

    move locals

--- app/overrides/pages_in_header.rb
+++ app/overrides/pages_in_header.rb
@@ -2,6 +2,6 @@ Deface::Override.new(
   virtual_path: 'spree/shared/_main_nav_bar',
   name: 'pages_in_header',
   original: 'b8836a13fdf734b8e6d4d0a1fbb7de541f17e44c' ,
-  insert_top: '#main-nav-bar > ul:first-child',
+  insert_top: '#main-sidebar,
   partial: 'spree/static_content/static_content_header'
 )
