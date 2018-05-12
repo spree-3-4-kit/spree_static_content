5a64b6d1d6f566bcbe813552fd16c36898580f02
Matthew Kennedy <m.kennedy@me.com>
2018-05-12 21:40:16 +0100

    rails

--- app/overrides/pages_in_header.rb
+++ app/overrides/pages_in_header.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(
-  virtual_path: 'spree/shared/_mmenu',
+  virtual_path: 'spree/shared/_mmenux',
   name: 'pages_in_header',
   original: 'b8836a13fdf734b8e6d4d0a1fbb7de541f17e44c' ,
   insert_top: '#main-sidebar',
--- app/overrides/pages_in_sidebar.rb
+++ app/overrides/pages_in_sidebar.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(
-  virtual_path: 'spree/shared/_mmenu',
+  virtual_path: 'spree/shared/_mmenux',
   name: 'pages_in_sidebar',
   original: '694724a574c0c4a7550745b3ae50d0c9633b2dbf',
   insert_bottom: '#sub-side',
