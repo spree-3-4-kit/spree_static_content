39de5a22628c79a249e372c8f6853c7c9f7ed28f
Matthew Kennedy <m.kennedy@me.com>
2018-05-12 21:26:46 +0100

    Try

--- app/overrides/pages_in_footer.rb
+++ app/overrides/pages_in_footer.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/shared/_mmenu',
   name: 'pages_in_footer',
-  insert_bottom: '#store-policies',
+  insert_bottom: '#footer-links',
   partial: 'spree/static_content/static_content_footer'
 )
